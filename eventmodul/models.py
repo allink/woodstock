4fdecd41772adb330d6b3bea1aa723c6a747d8cd
Marc Egli <frog32@me.com>
2011-01-19 15:18:37 +0100

    reordered admin and models file, added slug to events

--- eventmodul/models.py
+++ eventmodul/models.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.conf import settings
+from django.contrib import admin
 from django.contrib.auth import login as auth_login, authenticate
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
@@ -7,11 +9,14 @@ from django.core.urlresolvers import reverse
 from django.core import exceptions
 from django.db import models
 from django.db.models import Q
+from django.http import HttpResponseRedirect, HttpResponse
 from django.utils import translation
 from django.utils.translation import ugettext_lazy as _
 from django.template import loader, Context, Template
+from django.template.defaultfilters import slugify
 
-from pennyblack.options import NewsletterReceiverMixin, JobUnitMixin
+from pennyblack.options import NewsletterReceiverMixin, JobUnitMixin, \
+    JobUnitAdmin
 
 from eventmodul import settings
 
@@ -19,9 +24,18 @@ from feincms.module.medialibrary.models import MediaFile
 from feincms.translations import TranslatedObjectMixin, Translation, \
     TranslatedObjectManager
 
-import datetime
 import random
 import sys
+import csv
+from datetime import datetime
+
+try:
+    import xlwt
+except ImportError:
+    EXCEL_EXPORT_ACTIVE = False
+else:
+    EXCEL_EXPORT_ACTIVE = True
+
 
 class ExtendableMixin(object):
     @classmethod
@@ -33,16 +47,72 @@ class ExtendableMixin(object):
         """
         register_fn(cls, None)
     
+#-----------------------------------------------------------------------------
+# EventPart
+#-----------------------------------------------------------------------------
+class EventPartManager(models.Manager):
+    use_for_related_fields = True
+    def active(self):
+        """
+        Filters all active Eventparts
+        """
+        return self.filter(active=True)
 
-class EventManager(models.Manager):
+class EventPart(models.Model):
+    event = models.ForeignKey('Event', related_name="parts")
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    signable = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+    maximum_participants = models.IntegerField(default=0,
+        verbose_name="Maximum Participants",
+        help_text="Number of maximum participants or 0 for no limit.")
+
+    objects = EventPartManager()
+
+    class Meta:
+        ordering = ('date',)
+        verbose_name = "Event Part"
+        verbose_name_plural = "Event Parts"
+
+    def __unicode__(self):
+        return "%s %s" % (self.event, self.name)
+
+    def fully_booked(self):
+        """Returns true if event is already fully booked"""
+        if self.maximum_participants == 0:
+            return False
+        return self.get_participant_count() >= self.maximum_participants
+
+    def get_participant_count(self):
+        return self.participants.count()
+    get_participant_count.short_description = "Participants"
+
+class EventPartInline(admin.TabularInline):
+    model = EventPart
+    extra = 0
+
+#-----------------------------------------------------------------------------
+# Event
+#-----------------------------------------------------------------------------
+class EventManager(models.Manager, ExtendableMixin):
     def active(self):
         """
         Filters all active Events
         """
         return self.filter(active=True)
+    
+    def get_by_slug(self, slug, active=True):
+        """
+        Gets the event corresponding to slug
+        """
+        if not active:
+            return self.get(slug=slug)
+        return self.active().get(slug=slug)
 
-class Event(models.Model, TranslatedObjectMixin, JobUnitMixin):
+class Event(models.Model, TranslatedObjectMixin, JobUnitMixin, ExtendableMixin):
     active = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True)
     
     objects = EventManager()
     class Meta:
@@ -91,52 +161,62 @@ class Event(models.Model, TranslatedObjectMixin, JobUnitMixin):
             return
         self.translation.unsubscribe_mail.send(participant, group=self)
 
+    @classmethod
+    def register_extension(cls, register_fn):
+        register_fn(cls, EventAdmin, EventTranslation, EventTranslationInline, EventPart, EventPartInline)
+
 class EventTranslation(Translation(Event)):
     name = models.CharField(max_length=100)
     subscribe_mail = models.ForeignKey('pennyblack.Newsletter', blank=True,
         null=True, related_name="signup_events")
     unsubscribe_mail = models.ForeignKey('pennyblack.Newsletter', blank=True,
         null=True, related_name="signoff_events")
-    
 
-class EventPartManager(models.Manager):
-    use_for_related_fields = True
-    def active(self):
-        """
-        Filters all active Eventparts
-        """
-        return self.filter(active=True)
+class EventTranslationInline(admin.StackedInline):
+    model = EventTranslation
+    max_num = len(settings.LANGUAGES)
 
-class EventPart(models.Model):
-    event = models.ForeignKey(Event, related_name="parts")
-    name = models.CharField(max_length=100)
-    date = models.DateTimeField()
-    signable = models.BooleanField(default=False)
-    active = models.BooleanField(default=True)
-    maximum_participants = models.IntegerField(default=0,
-        verbose_name="Maximum Participants",
-        help_text="Number of maximum participants or 0 for no limit.")
-    
-    objects = EventPartManager()
+class EventAdmin(JobUnitAdmin):
+    inlines = (EventTranslationInline, EventPartInline,)
+    list_display = ('__unicode__', 'active', 'get_participant_count')
+    collection_selection_form_extra_fields = {
+        'attended': forms.BooleanField(required=False),
+        'confirmed': forms.BooleanField(required=False),
+    }
 
-    class Meta:
-        ordering = ('date',)
-        verbose_name = "Event Part"
-        verbose_name_plural = "Event Parts"
-    
-    def __unicode__(self):
-        return "%s %s" % (self.event, self.name)
+    def tool_excel_export(self, request, obj, button):
+        response = HttpResponse(mimetype="application/ms-excel")
+        response['Content-Disposition'] = 'attachment; filename=%s.xls' % (slugify(obj.name),)
 
-    def fully_booked(self):
-        """Returns true if event is already fully booked"""
-        if self.maximum_participants == 0:
-            return False
-        return self.get_participant_count() >= self.maximum_participants
-    
-    def get_participant_count(self):
-        return self.participants.count()
-    get_participant_count.short_description = "Participants"
+        wb = xlwt.Workbook()
+        ws = wb.add_sheet(slugify(obj.name))
+        row = 0
 
+        ws.write(row, 0, 'Firstname')
+        ws.write(row, 1, 'Surname')
+        ws.write(row, 2, 'Company')
+        ws.write(row, 3, 'Location')
+        ws.write(row, 4, 'Email')
+        ws.write(row, 5, 'Language')
+        ws.write(row, 6, 'Registred')
+        row += 1
+
+        for participant in obj.participants.all():
+            ws.write(row, 0, participant.firstname)
+            ws.write(row, 1, participant.surname)
+            ws.write(row, 2, participant.company)
+            ws.write(row, 3, participant.location)
+            ws.write(row, 4, participant.email)
+            ws.write(row, 5, participant.language)
+            ws.write(row, 6, participant.date_registred.strftime('%d.%m.%Y %H:%M'))
+            row += 1
+
+        wb.save(response)
+        return response
+
+#-----------------------------------------------------------------------------
+# Group
+#-----------------------------------------------------------------------------
 class Group(models.Model, JobUnitMixin):
     name = models.CharField(max_length=100)
     event = models.ForeignKey(Event, null=True, blank=True, default=None,
@@ -152,16 +232,63 @@ class Group(models.Model, JobUnitMixin):
     def get_customer_count(self):
         return str(self.customers.count())
     get_customer_count.short_description = "Customers"
-        
 
-class Person(models.Model, NewsletterReceiverMixin):
+class GroupAdminForm(forms.ModelForm):
+    import_field = forms.FileField(label="CSV import", required=False)
+    language = forms.TypedChoiceField(choices=settings.LANGUAGES, 
+        initial=settings.LANGUAGE_CODE)
+    class Meta:
+        model = Group
+
+    def save(self, commit=True):
+        model = super(GroupAdminForm, self).save(commit=False)
+        model.save()
+        if self.cleaned_data['import_field']:
+            reader = csv.reader(self.cleaned_data['import_field'], delimiter=';')
+            for row in reader:
+                if row[0] == 'Herr':
+                    salutation = 1
+                elif row[0] == 'Frau':
+                    salutation = 2
+                else:
+                    salutation = 0
+                invitee = Invitee(
+                    salutation=salutation,
+                    title=row[1],
+                    firstname=row[2],
+                    surname=row[3],
+                    company=row[4],
+                    location=row[5],
+                    email=row[6],
+                    language=self.cleaned_data['language'],
+                )
+                invitee.save()
+                invitee.groups.add(model)
+        return model
+
+class GroupAdmin(JobUnitAdmin):
+    list_display = ('name', 'get_customer_count',)
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'event',),
+        }),
+        ('Add Customers', {
+            'fields': ('import_field', 'language',),
+        }),
+    )
+    form = GroupAdminForm                
+
+#-----------------------------------------------------------------------------
+# Person
+#-----------------------------------------------------------------------------
+class Person(models.Model, NewsletterReceiverMixin, ExtendableMixin):
     salutation = models.ForeignKey('Salutation')
     firstname = models.CharField(verbose_name=_('Firstname'), max_length=100)
     surname = models.CharField(verbose_name=_('Surname'), max_length=100)
     email = models.EmailField(verbose_name=_('E-Mail'))
     password = models.CharField(verbose_name=_('Password'),max_length=100)
     is_active = models.BooleanField(verbose_name=_('Active'), default=False)
-    last_login = models.DateTimeField(_('last login'), default=datetime.datetime.now)
+    last_login = models.DateTimeField(_('last login'), default=datetime.now)
     language = models.CharField(max_length=5, choices=settings.LANGUAGES, 
         default=settings.LANGUAGE_CODE, blank=True)    
     
@@ -199,7 +326,33 @@ class Person(models.Model, NewsletterReceiverMixin):
         user = authenticate(user=self)
         if user:
             auth_login(request, user)
-            
+
+    def activate(self):
+        self.is_active = True
+        self.save()
+
+#-----------------------------------------------------------------------------
+# Attendance
+#-----------------------------------------------------------------------------
+class Attendance(models.Model):
+    participant = models.ForeignKey('Participant', related_name='attendances')
+    event_part = models.ForeignKey('EventPart', related_name='attendances')
+    confirmed = models.BooleanField(default=False)
+    attended = models.BooleanField(default=True)
+    date_registred = models.DateTimeField(default=datetime.now(), verbose_name="Signup Date")
+
+    def __unicode__(self):
+        return '%s is attending %s' % (self.participant, self.event_part)    
+
+class AttendanceInline(admin.TabularInline):
+    model = Attendance
+    extra = 0
+    readonly_fields = ('date_registred',)
+
+
+#-----------------------------------------------------------------------------
+# Participant
+#-----------------------------------------------------------------------------
 class Participant(Person):
     
     event_parts = models.ManyToManyField('EventPart', related_name="participants",
@@ -240,18 +393,21 @@ class Participant(Person):
         if settings.SUBSCRIPTION_NEEDS_INVITATION and self.invitee is None:
             raise exceptions.PermissionDenied("Only invited Persons can register.")
         super(Participant,self).save(*args, **kwargs)
-            
-
-class Attendance(models.Model):
-    participant = models.ForeignKey('Participant', related_name='attendances')
-    event_part = models.ForeignKey('EventPart', related_name='attendances')
-    confirmed = models.BooleanField(default=False)
-    attended = models.BooleanField(default=True)
-    date_registred = models.DateTimeField(default=datetime.datetime.now(), verbose_name="Signup Date")
     
-    def __unicode__(self):
-        return '%s is attending %s' % (self.participant, self.event_part)    
+    @classmethod
+    def register_extension(cls, register_fn):
+        register_fn(cls, ParticipantAdmin)
+            
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'surname', 'email', 'language',)
+    list_filter   = ('language', 'event_parts',)
+    readonly_fields = ('invitee',)
+    inlines = (AttendanceInline,)
 
+
+#-----------------------------------------------------------------------------
+# Invitee
+#-----------------------------------------------------------------------------
 class Invitee(Person):
     groups = models.ManyToManyField(Group, related_name="customers", blank=True)
 
@@ -260,6 +416,14 @@ class Invitee(Person):
         verbose_name_plural = 'Einladungen'
         ordering = ('surname',)
 
+class InviteeAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'surname', 'email', 'language',)
+    list_filter   = ('language', 'groups',)
+
+
+#-----------------------------------------------------------------------------
+# Salutation
+#-----------------------------------------------------------------------------
 class SalutationManager(models.Manager):
     def localized(self):
         """
@@ -279,5 +443,8 @@ class Salutation(models.Model):
     
     def __unicode__(self):
         return self.text
-    
+
+
+class SalutationAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'gender')
     
