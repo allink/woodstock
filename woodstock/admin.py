78f115cf51bf223230a4826a59be904ef5b2d422
Marc Egli <frog32@me.com>
2011-02-09 13:56:39 +0100

    add setting to hide some admin parts

--- woodstock/admin.py
+++ woodstock/admin.py
@@ -1,11 +1,17 @@
 from woodstock.models import Event, EventAdmin,\
     Participant, ParticipantAdmin, Invitee, InviteeAdmin, Group, GroupAdmin,\
     Salutation, SalutationAdmin
+from woodstock import settings
 
 from django.contrib import admin
 
-admin.site.register(Event, EventAdmin)
-admin.site.register(Participant, ParticipantAdmin)
-admin.site.register(Group,GroupAdmin)
-admin.site.register(Invitee, InviteeAdmin)
-admin.site.register(Salutation, SalutationAdmin)
+if settings.ADMIN_HAS_EVENT:
+    admin.site.register(Event, EventAdmin)
+if settings.ADMIN_HAS_PARTICIPANT:
+    admin.site.register(Participant, ParticipantAdmin)
+if settings.ADMIN_HAS_GROUP:
+    admin.site.register(Group,GroupAdmin)
+if settings.ADMIN_HAS_INVITEE:
+    admin.site.register(Invitee, InviteeAdmin)
+if settings.ADMIN_HAS_SALUTATION:
+    admin.site.register(Salutation, SalutationAdmin)
