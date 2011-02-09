2a78c6f71a6b4f9fdde73bdf1144696212c38487
Marc Egli <frog32@me.com>
2011-02-09 11:08:25 +0100

    added password only login form

--- woodstock/forms.py
+++ woodstock/forms.py
@@ -62,3 +62,45 @@ class RegisterForm(forms.ModelForm):
         if not participant.is_active:
             send_newsletter(settings.ACTIVATION_NEWSLETTER, participant)
         return participant
+
+class CodeAuthenticationForm(forms.Form):
+    """
+    Base class for authenticating users. Extend this to get a form that accepts
+    username/password logins.
+    """
+    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+
+    def __init__(self, request=None, *args, **kwargs):
+        """
+        If request is passed in, the form will validate that cookies are
+        enabled. Note that the request (a HttpRequest object) must have set a
+        cookie with the key TEST_COOKIE_NAME and value TEST_COOKIE_VALUE before
+        running this validation.
+        """
+        self.request = request
+        self.user_cache = None
+        super(CodeAuthenticationForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        password = self.cleaned_data.get('password')
+
+        if password:
+            self.user_cache = authenticate(username=None, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(_("Please enter a correct username and password. Note that both fields are case-sensitive."))
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError(_("This account is inactive."))
+
+        if self.request:
+            if not self.request.session.test_cookie_worked():
+                raise forms.ValidationError(_("Your Web browser doesn't appear to have cookies enabled. Cookies are required for logging in."))
+
+        return self.cleaned_data
+
+    def get_user_id(self):
+        if self.user_cache:
+            return self.user_cache.id
+        return None
+
+    def get_user(self):
+        return self.user_cache
