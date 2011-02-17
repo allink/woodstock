bef8ba5874443dc4793cc431f593f26342ec3a3d
Marc Egli <frog32@me.com>
2011-02-17 16:37:45 +0100

    added next events content type

--- woodstock/content/event/models.py
+++ woodstock/content/event/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
+from woodstock.models import Event
+
 class NextEventsContent(models.Model):
     count = models.IntegerField(default=5)
     
@@ -11,5 +13,5 @@ class NextEventsContent(models.Model):
         verbose_name_plural = 'Next Events Contents'
     
     def render(self, **kwargs):
-        events = Event.objects.pending()[self.count:]
+        events = Event.objects.pending()[0:self.count]
         return render_to_string('content/woodstock/next_events.html',{'events':events})
