231c46e9e98d15cfdd310b88ed3c225e1e02c08d
Marc Egli <frog32@me.com>
2011-07-21 15:18:09 +0200

    update example to django 1.3

--- example/models.py
+++ example/models.py
@@ -1,3 +1,23 @@
+# feincms stuff
+from feincms.module.page.models import Page
+from feincms.content.richtext.models import RichTextContent
+from feincms.content.application.models import ApplicationContent
+
+Page.register_templates({
+    'key': 'base',
+    'title': 'Basic Template',
+    'path': 'base.html',
+    'regions': (
+        ('main', 'Main Region'),
+    ),
+})
+
+Page.create_content_type(RichTextContent)
+Page.create_content_type(ApplicationContent, APPLICATIONS=(
+    ('woodstock.urls.simple', 'Woodstock Simple'),
+))
+
+# pennyblack stuff
 from pennyblack.models import Newsletter
 from pennyblack.content.richtext import TextOnlyNewsletterContent, \
     TextWithImageNewsletterContent
@@ -8,8 +28,8 @@ Newsletter.register_templates({
     'path': 'base_newsletter.html',
     'regions': (
         ('main', 'Main Region'),
-        ),
-    })
+    ),
+})
     
 Newsletter.create_content_type(TextOnlyNewsletterContent)
 Newsletter.create_content_type(TextWithImageNewsletterContent)
