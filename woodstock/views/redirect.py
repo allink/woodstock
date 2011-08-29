b0abceac46e657c6a11fcd7ac8bd3ec0eda7fd5b
Marc Egli <frog32@me.com>
2011-08-29 18:06:58 +0200

    add redirect view to redirect to the next upcoming event

---
+++ woodstock/views/redirect.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponseRedirect
+
+from woodstock import settings
+from woodstock.models import Event
+
+try:
+    from feincms.content.application.models import reverse
+except ImportError:
+    from django.core.urlresolvers import reverse
+    
+def next_active_event(request):
+    try:
+        event = Event.objects.pending()[0]
+    except IndexError:
+        try:
+            event = Event.objects.active()[0]
+        except IndexError:
+            return HttpResponseRedirect('/')
+    return HttpResponseRedirect(reverse(settings.EVENT_DETAIL_URL_NAME, args=(event.slug,)))
